2614b8724f792116fcef650871cd8a1d02e5da3f
zied gandouzi <ziedgandouzi@gmail.com>
2023-05-24 19:36:32 +0100

    first commit

---
+++ account/forms.py
@@ -0,0 +1,186 @@
+import os
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import FileExtensionValidator
+
+from account.models import User
+
+
+class EmployeeRegistrationForm(UserCreationForm):
+    cv = forms.FileField(label="Upload CV", required=False, validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
+    def __init__(self, *args, **kwargs):
+        UserCreationForm.__init__(self, *args, **kwargs)
+        self.fields['gender'].required = True
+        self.fields['first_name'].label = "First Name :"
+        self.fields['last_name'].label = "Last Name :"
+        self.fields['password1'].label = "Password :"
+        self.fields['password2'].label = "Confirm Password :"
+        self.fields['email'].label = "Email :"
+        self.fields['cv'].label = "Add Your CV (PDF) :"
+        self.fields['gender'].label = "Gender :"
+
+        self.fields['cv'].widget.attrs.update({'accept': 'application/pdf'})  # Specify accepted file types
+
+        self.fields['first_name'].widget.attrs.update(
+            {
+                'placeholder': 'Enter First Name',
+            }
+        )
+        self.fields['last_name'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Last Name',
+            }
+        )
+        self.fields['email'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Email',
+            }
+        )
+        self.fields['password1'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Password',
+            }
+        )
+        self.fields['password2'].widget.attrs.update(
+            {
+                'placeholder': 'Confirm Password',
+            }
+        )
+
+    class Meta:
+
+        model=User
+
+        fields = ['first_name', 'last_name', 'email','cv', 'password1', 'password2', 'gender']
+
+    def clean_cv(self):
+        cv = self.cleaned_data.get('cv')
+        if cv:
+            if cv.size > 5242880:  # Specify maximum file size in bytes (e.g., 5MB)
+                raise forms.ValidationError('The file size exceeds the limit.')
+        return cv
+    def clean_gender(self):
+        gender = self.cleaned_data.get('gender')
+        if not gender:
+            raise forms.ValidationError("Gender is required")
+        return gender
+
+    def save(self, commit=True):
+        user = UserCreationForm.save(self,commit=False)
+        user.role = "employee"
+        if commit:
+            user.save()
+        return user
+
+
+class EmployerRegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        UserCreationForm.__init__(self, *args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['first_name'].label = "Company Name"
+        self.fields['last_name'].label = "Company Address"
+        self.fields['password1'].label = "Password"
+        self.fields['password2'].label = "Confirm Password"
+
+        self.fields['first_name'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Company Name',
+            }
+        )
+        self.fields['last_name'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Company Address',
+            }
+        )
+        self.fields['email'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Email',
+            }
+        )
+        self.fields['password1'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Password',
+            }
+        )
+        self.fields['password2'].widget.attrs.update(
+            {
+                'placeholder': 'Confirm Password',
+            }
+        )
+    class Meta:
+
+        model=User
+
+        fields = ['first_name', 'last_name','email', 'password1', 'password2',]
+
+
+    def save(self, commit=True):
+        user = UserCreationForm.save(self,commit=False)
+        user.role = "employer"
+        if commit:
+            user.save()
+        return user
+
+
+class UserLoginForm(forms.Form):
+    email =  forms.EmailField(
+    widget=forms.EmailInput(attrs={ 'placeholder':'Email',})
+) 
+    password = forms.CharField(strip=False,widget=forms.PasswordInput(attrs={
+        
+        'placeholder':'Password',
+    }))
+
+    def clean(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        if email and password:
+            self.user = authenticate(email=email, password=password)
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                raise forms.ValidationError("User Does Not Exist.")
+
+            if not user.check_password(password):
+                raise forms.ValidationError("Password Does not Match.")
+
+            if not user.is_active:
+                raise forms.ValidationError("User is not Active.")
+            if  user.is_staff:
+                raise forms.ValidationError("User Does Not Exist.")
+
+        return super(UserLoginForm, self).clean(*args, **kwargs)
+
+    def get_user(self):
+        return self.user
+
+
+
+class EmployeeProfileEditForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(EmployeeProfileEditForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs.update(
+            {
+                'placeholder': 'Enter First Name',
+            }
+        )
+        self.fields['last_name'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Last Name',
+            }
+        )
+        self.fields['cv'].widget.attrs.update(
+            {
+                'placeholder': 'Add CV',
+            }
+        )
+        
+        
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name","cv","gender"]
---
+++ account/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from account import views
+from django.contrib import admin
+
+
+app_name = "account"
+
+urlpatterns = [
+
+    path('employee/register/', views.employee_registration, name='employee-registration'),
+    path('employer/register/', views.employer_registration, name='employer-registration'),
+    path('profile/edit/<int:id>/', views.employee_edit_profile, name='edit-profile'),
+    path('login/', views.user_logIn, name='login'),
+    path('logout/', views.user_logOut, name='logout'),
+
+    
+    path('admin/', admin.site.urls),
+]
